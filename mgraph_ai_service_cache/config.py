2993ca9c16b0687739d033ec3bec899ab5b9f51c
Dinis Cruz <dinis.cruz@owasp.org>
2025-08-29 11:54:53 +0100

    updated 'osbot-fast-api-serverless==v1.16.0'

--- mgraph_ai_service_cache/config.py
+++ mgraph_ai_service_cache/config.py
@@ -3,5 +3,5 @@ from mgraph_ai_service_cache import package_name
 SERVICE_NAME                             = package_name
 FAST_API__TITLE                          = "MGraph-AI Service Cache"
 FAST_API__DESCRIPTION                    = "Cache Service"
-LAMBDA_DEPENDENCIES__FAST_API_SERVERLESS = ['osbot-fast-api-serverless==v1.15.0',
+LAMBDA_DEPENDENCIES__FAST_API_SERVERLESS = ['osbot-fast-api-serverless==v1.16.0',
                                             'memory-fs==0.17.0'                 ]
