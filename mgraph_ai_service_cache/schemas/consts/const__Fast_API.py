54288614d8968fad6d5d7ca2be716046f5c338a4
Dinis Cruz <dinis.cruz@owasp.org>
2025-09-20 01:55:20 +0100

    minor fix

--- mgraph_ai_service_cache/schemas/consts/const__Fast_API.py
+++ mgraph_ai_service_cache/schemas/consts/const__Fast_API.py
@@ -1,7 +1,7 @@
 from fastapi                                                     import Path
 from mgraph_ai_service_cache.schemas.cache.consts__Cache_Service import DEFAULT_CACHE__NAMESPACE
 
-FAST_API__PARAM__NAMESPACE                  = Path(..., example=DEFAULT_CACHE__NAMESPACE )
+FAST_API__PARAM__NAMESPACE                  = Path(..., examples=DEFAULT_CACHE__NAMESPACE )
 ENV_VAR__CACHE__SERVICE__BUCKET_NAME        = 'CACHE__SERVICE__BUCKET_NAME'
 ENV_VAR__CACHE__SERVICE__DEFAULT_TTL_HOURS  = 'CACHE__SERVICE__DEFAULT_TTL_HOURS'
 
