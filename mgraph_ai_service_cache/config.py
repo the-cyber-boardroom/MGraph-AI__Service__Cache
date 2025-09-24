94a7e8609c2ff27d4904b92f378e82eb3a9f481b
Dinis Cruz <dinis.cruz@owasp.org>
2025-09-24 01:11:46 +0100

    updated memory-fs ref

--- mgraph_ai_service_cache/config.py
+++ mgraph_ai_service_cache/config.py
@@ -4,4 +4,4 @@ SERVICE_NAME                             = package_name
 FAST_API__TITLE                          = "MGraph-AI Service Cache"
 FAST_API__DESCRIPTION                    = "Cache Service"
 LAMBDA_DEPENDENCIES__FAST_API_SERVERLESS = ['osbot-fast-api-serverless==v1.22.0',
-                                            'memory-fs==v0.31.0'                ]
+                                            'memory-fs==v0.34.0'                ]
