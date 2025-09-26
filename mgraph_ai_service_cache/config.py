959889709c9c4ac66c6127fb4b2cd5f0d2cea398
Dinis Cruz <dinis.cruz@owasp.org>
2025-09-27 00:53:43 +0100

    added mgraph-ai-service-cache-client dependency, which is the one that has the schemas

--- mgraph_ai_service_cache/config.py
+++ mgraph_ai_service_cache/config.py
@@ -3,5 +3,6 @@ from mgraph_ai_service_cache import package_name
 SERVICE_NAME                             = package_name
 FAST_API__TITLE                          = "MGraph-AI Service Cache"
 FAST_API__DESCRIPTION                    = "Cache Service"
-LAMBDA_DEPENDENCIES__FAST_API_SERVERLESS = ['osbot-fast-api-serverless==v1.22.0',
-                                            'memory-fs==v0.35.0'                ]
+LAMBDA_DEPENDENCIES__FAST_API_SERVERLESS = ['osbot-fast-api-serverless==v1.22.0'    ,
+                                            'memory-fs==v0.35.0'                    ,
+                                            'mgraph-ai-service-cache-client==v0.1.3']
