4889d18c9bfd79de808ef74775dee89bfd0a70e4
Dinis Cruz <dinis.cruz@owasp.org>
2025-10-12 02:06:36 +0100

    updated client version

--- mgraph_ai_service_cache/config.py
+++ mgraph_ai_service_cache/config.py
@@ -5,4 +5,4 @@ FAST_API__TITLE                          = "MGraph-AI Service Cache"
 FAST_API__DESCRIPTION                    = "Cache Service"
 LAMBDA_DEPENDENCIES__FAST_API_SERVERLESS = ['osbot-fast-api-serverless==v1.23.0'    ,
                                             'memory-fs==v0.35.0'                    ,
-                                            'mgraph-ai-service-cache-client==v0.1.3']
+                                            'mgraph-ai-service-cache-client==v0.5.0']
