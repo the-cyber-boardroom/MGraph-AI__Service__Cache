33531774448d786b07f22b6ba6b33501d76adee3
Dinis Cruz <dinis.cruz@owasp.org>
2026-01-19 12:47:34 +0000

    updated memory-fs dependency

--- mgraph_ai_service_cache/config.py
+++ mgraph_ai_service_cache/config.py
@@ -3,8 +3,8 @@ from mgraph_ai_service_cache import package_name
 SERVICE_NAME                             = package_name
 FAST_API__TITLE                          = "MGraph-AI Service Cache"
 FAST_API__DESCRIPTION                    = "Cache Service"
-LAMBDA_DEPENDENCIES__FAST_API_SERVERLESS = ['memory-fs==v0.39.0'                     ,
-                                            'mgraph-ai-service-cache-client==v0.24.0',
+LAMBDA_DEPENDENCIES__FAST_API_SERVERLESS = ['memory-fs==v0.40.0'                     ,
+                                            'mgraph-ai-service-cache-client==v0.27.0',
                                             'osbot-fast-api-serverless==1.33.0'      ]
 
 CACHE_SERVICE__WEB_CONSOLE__PATH              = 'console'
